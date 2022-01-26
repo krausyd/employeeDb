b9ae51f4679dd756112dac8909e9e766afb8f709
krausyd <krausyd@hotmail.com>
2022-01-25 19:48:00 -0800

    (∩◕-◕ )⊃━✿✿✿✿ add getting deparment, and cycling through the menu

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const inquirer = require('inquirer');
+const getAllDeparments = require('./lib/department');
 
 const mainQuestionOptions = [
-    'view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'
+    'view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role', 'leave'
 ]
 const mainQuestion = {
-    name: 'menu',
+    name: 'option',
     message: 'What would you like to do?',
     type: 'list',
     choices: mainQuestionOptions
@@ -14,12 +15,32 @@ const promptWhatToDo = () => {
     return inquirer.prompt(mainQuestion);
 }
 
-const showNext = (option) => {
-    console.log(option);
+const showNext = ({ option }) => {
+    switch (option) {
+        case 'view all departments':
+            getAllDeparments().catch(err => console.log('An error ocurred, try again later')).then(showMainMenu);
+            break;
+        case 'view all roles':
+            break;
+        case 'view all employees':
+            break;
+        case 'add a department':
+            break;
+        case 'add a role':
+            break;
+        case 'add an employee':
+            break;
+        case 'update an employee role':
+            break;
+        case 'leave':
+            console.log('Thanks for using the system!');
+            break;
+    }
 }
 
 const showMainMenu = () => {
     promptWhatToDo().then(showNext);
 }
 
+console.log('Welcome to the application employeesDb');
 showMainMenu();
